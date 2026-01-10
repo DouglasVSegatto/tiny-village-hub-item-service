b012adaa6034c3341462a8b0755dcabd64096567
Segatto <78928583+DouglasVSegatto@users.noreply.github.com>
2026-01-09 23:32:02 -0700

    updated env variables and set EnableConfigurationProperties

--- src/main/java/com/segatto_builder/tinyvillagehubitemservice/TinyVillageHubItemServiceApplication.java
+++ src/main/java/com/segatto_builder/tinyvillagehubitemservice/TinyVillageHubItemServiceApplication.java
@@ -2,10 +2,12 @@ package com.segatto_builder.tinyvillagehubitemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableMongoAuditing
+@EnableConfigurationProperties
 public class TinyVillageHubItemServiceApplication {
 
     public static void main(String[] args) {
